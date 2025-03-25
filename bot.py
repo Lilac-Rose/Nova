ea1f38453e1a522b847ed1e1acbcb2ba007146eb
Lilac <ellaevalon122342@gmail.com>
2025-03-25 17:20:46 +0100

    Changed prefix from ? to ++

--- bot.py
+++ bot.py
@@ -104,19 +104,26 @@ class MyBot(commands.Bot):
         await close_pool()
         await super().close()
 
+@commands.command()
+async def sync(ctx):
+    await ctx.bot.tree.sync()
+    await ctx.send("Slash commands synced!")
+
 intents = discord.Intents.default()
 intents.messages = True
 intents.message_content = True
 intents.members = True
 
 bot = MyBot(
-    command_prefix="!",
+    command_prefix="++",
     intents=intents,
     activity=discord.Activity(
         type=discord.ActivityType.watching,
         name=f"{status} | Pronouns: {pronouns}"
     )
 )
+
+bot.add_command(sync)
 
 if __name__ == "__main__":
     bot.run(os.getenv('TOKEN'))
